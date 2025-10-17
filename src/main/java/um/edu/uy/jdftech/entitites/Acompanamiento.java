88cb8f8a46342600c378cfab9a5114b22079b68f
parket453 <parket453@gmail.com>
2025-10-17 09:40:12 -0300

    Arranqué Pedido, MedioDePago, Usuario, Cliente, y Administrador

---
+++ src/main/java/um/edu/uy/jdftech/entitites/Acompanamiento.java
@@ -0,0 +1,16 @@
+package um.edu.uy.jdftech.entitites;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "acompanamientos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Acompanamiento {
+
+}
