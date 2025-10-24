d8d638d90eec9d73c6fb60949b86eca794fb898a
ElPapinha <francopapasilva@gmail.com>
2025-10-23 22:50:11 -0300

    inicie las clases -Aderezo, Topping, Hamburguesa, Pizza- con columnas y relaciones entre si

---
+++ src/main/java/um/edu/uy/jdftech/entitites/Aderezo.java
@@ -0,0 +1,41 @@
+package um.edu.uy.jdftech.entitites;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "aderezos")
+public class Aderezo {
+
+    @Id
+    @GeneratedValue(generator = "aderezos_ids")
+    @GenericGenerator(name = "aderezos_ids", strategy = "increment")
+    @Column(name = "id_aderezo")
+    private Long id_aderezo;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "precio")
+    private double precio;
+
+    @ManyToOne
+    @JoinColumn(name = "id_hamburguesa")
+    private Hamburguesa hamburguesa;
+
+    public Aderezo() {
+    }
+
+    public Aderezo(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Long getIdAderezo() {
+        return id_aderezo;
+    }
+
+    public void setIdAderezo(Long id_aderezo) {
+        this.id_aderezo = id_aderezo;
+    }
+}
